a19969087ea17dcc199ad5161841a2f2ac6080d8
OisinMoran <oisinjtmoran@gmail.com>
2017-09-23 02:14:19 +0100

    Added input box

--- articles/next_in_sequence.js
+++ articles/next_in_sequence.js
@@ -1,3 +1,22 @@
+function round(value, exp) {
+  if (typeof exp === 'undefined' || +exp === 0)
+    return Math.round(value);
+
+  value = +value;
+  exp = +exp;
+
+  if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0))
+    return NaN;
+
+  // Shift
+  value = value.toString().split('e');
+  value = Math.round(+(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp)));
+
+  // Shift back
+  value = value.toString().split('e');
+  return +(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp));
+}
+
 function print(message, id) {
 	const output = document.getElementById(id);
 	output.innerHTML = message;
@@ -26,6 +45,7 @@ function find_polynomial(sequence) {
 	return math.lusolve(M, sequence);
 }
 
+// TO DO: Fix this to deal with exponential form (e.g. input of "1,2,4,8" gives v small number)
 function print_polynomial(coeffs) {
 	let html = "$$f(x)=";
 	let leading_flag = true;
@@ -45,12 +65,16 @@ function print_polynomial(coeffs) {
 			continue;
 		}
 
-		if (exp === 0) {
-			html += sign_format(coeff, leading_flag) + coeff;
+		if (exp === 0 && leading_flag === true) {
+			html += round(coeff, 4);
+			leading_flag = false;
+			continue;
+		} else if (exp === 0){
+			html += sign_format(coeff, leading_flag) + round(coeff, 4);
 			leading_flag = false;
 			continue;
 		} 
-		html += sign_format(coeff, leading_flag) + coeff + "x^{" + exp + "}";
+		html += sign_format(coeff, leading_flag) + round(coeff, 4) + "x^{" + exp + "}";
 		leading_flag = false;
 	}
 	html += "$$";
@@ -61,4 +85,10 @@ function find_linear_recurrence(sequence) {
 	// TODO
 }
 
-print_polynomial(find_polynomial([4,16,64,256])); // 2^(2*n)
+function parse_and_solve() {
+	let sequence = document.getElementById("input1").value.split(/[ ,]+/);
+	sequence = sequence.filter(elem => elem.length > 0);
+	sequence = sequence.map(Number);
+	print_polynomial(find_polynomial(sequence)); // 4,16,64,256 -> 2^(2*n)
+	MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+}
